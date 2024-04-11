d84339e0f932f024f62e02266f958f5c9a731ff7
washingtonwei <weibingyang@gmail.com>
2024-04-11 07:28:29 -0500

    v-model vs v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>v-model vs v-bind</h1>
+  <h2>message: {{ message }}</h2>
+  <div>
+    <p>v-model = v-bind + @input</p>
+    <input
+      type="text"
+      v-bind:value="message"
+      @input="message = $event.target.value"
+    />
+  </div>
+  <div>
+    <p>v-model</p>
+    <input type="text" v-model="message" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<style lang="scss" scoped></style>
+let message = ref('Hello!')
+</script>
+
+<style scoped></style>
