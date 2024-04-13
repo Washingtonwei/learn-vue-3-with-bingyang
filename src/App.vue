552bc4fe70bba3af6452ceaa18dd758a057b8fdf
washingtonwei <weibingyang@gmail.com>
2024-04-13 08:44:18 -0500

    Lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,68 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>{{ message }}</h1>
+  <div class="card">
+    <h2 ref="title">This is the App component.</h2>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by one</button>
+    <button @click="isShow = !isShow">Toggle component1</button>
+    <Component1 v-if="isShow"></Component1>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  watch
+} from 'vue'
 
-<style lang="scss" scoped></style>
+import Component1 from './Component1.vue'
+
+let message = ref('Hello, Lifecycle Hooks!')
+
+let number = ref(1)
+let title = ref(null)
+
+let isShow = ref(true)
+
+console.log('App component is setup.')
+
+onBeforeMount(() => {
+  console.log('App component is before mount.')
+  console.log(number.value) // The ref data works.
+  console.log(title.value) // Since the component has not yet been mounted, you won't have access to the component's template or DOM elements within onBeforeMount.
+})
+onMounted(() => {
+  console.log('App component is mounted.')
+  console.log(title.value) // This works once the component is mounted.
+})
+onBeforeUpdate(() => {
+  console.log('App component is before update.')
+})
+onUpdated(() => {
+  console.log('App component is updated.')
+})
+onBeforeUnmount(() => {
+  console.log('App component is before unmount.')
+})
+onUnmounted(() => {
+  console.log('App component is unmounted.')
+})
+watch(number, () => {
+  console.log('number changes!')
+})
+</script>
+
+<style scoped>
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/Component1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="background-color: aqua; padding: 10px; margin: 20px">
+    <h2>This is the Component1 component.</h2>
+  </div>
+</template>
+
+<script setup>
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue'
+
+console.log('Component1 is setup.')
+
+onBeforeMount(() => {
+  console.log('Component1 is before mount.')
+})
+onMounted(() => {
+  console.log('Component1 is mounted.')
+})
+onBeforeUpdate(() => {
+  console.log('Component1 is before update.')
+})
+onUpdated(() => {
+  console.log('Component1 is updated.')
+})
+onBeforeUnmount(() => {
+  console.log('Component1 is before unmount.')
+})
+onUnmounted(() => {
+  console.log('Component1 is unmounted.')
+})
+</script>
