5318d638d568cb0b7f3a4eda03dc968bfd0f6a9f
washingtonwei <weibingyang@gmail.com>
2024-04-14 19:46:32 -0500

    Pass data to a parent component with custom events.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>Hello, Vue!</div>
+  <BlogPost
+    v-for="post in posts"
+    :key="post.id"
+    v-bind="post"
+    @delete-blog-post="processDeletion"
+  ></BlogPost>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import BlogPost from './BlogPost.vue'
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
+  }
+])
 
-<style lang="scss" scoped></style>
+// The second argument from the BlogPost’s emit method will be passed as the first parameter of this method.
+function processDeletion(id) {
+  let index = posts.value.findIndex((item) => item.id == id)
+  posts.value.splice(index, 1)
+}
+</script>
+
+<style scoped></style>
---
+++ src/BlogPost.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="blog-post">
+    <p>
+      {{ message }}
+    </p>
+    <h2>{{ id }} - {{ blogPostTitle }}</h2>
+    <h4>{{ blogPostContent }}</h4>
+    <button @click="$emit('delete-blog-post', id)">Delete post</button>
+    <button @click="emitDeletePostEvent(id)">Delete post</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('This is the BlogPost component.')
+defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+
+const emit = defineEmits(['delete-blog-post'])
+function emitDeletePostEvent(id) {
+  emit('delete-blog-post', id)
+}
+</script>
+
+<style scoped>
+.blog-post {
+  background-color: aqua;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
