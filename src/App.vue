04a55d352af88c55d0a1d5be278ad0af8ad98ad3
washingtonwei <weibingyang@gmail.com>
2024-04-16 09:06:21 -0500

    Vue named slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>Hello, Vue!</div>
+  <BaseLayout>
+    <template #header>
+      <h1>Here might be a page title.</h1>
+    </template>
+    <template #main>
+      <p>A paragraph of the main content.</p>
+      <p>And another one.</p>
+    </template>
+    <template #footer>
+      <p>Here's some contact info.</p>
+    </template>
+  </BaseLayout>
 </template>
 
-<script setup></script>
+<script setup>
+import BaseLayout from './BaseLayout.vue'
+</script>
 
 <style lang="scss" scoped></style>
---
+++ src/BaseLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <!-- We want header content here. -->
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <!-- We want main content here. -->
+      <slot name="main"></slot>
+    </main>
+    <footer>
+      <!-- We want footer content here. -->
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
